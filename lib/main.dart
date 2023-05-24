f2880e20e8ee15f618ede441e467b7fd45ca9ee6
jeminblack <jeminthesiya101@gmail.com>
2023-05-24 17:49:48 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:expence_tracker/screen/homeScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (p0) => HomeScreen(),
+      },
+    ),
+  );
+}
